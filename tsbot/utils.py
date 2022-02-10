2741a7271c1051e504e3ac12f92de51d596782a6
0x4aK <honttinen@gmail.com>
2022-02-10 14:49:26 +0200

    Added parse functions

--- tsbot/utils.py
+++ tsbot/utils.py
@@ -1,6 +1,24 @@
 from __future__ import annotations
 
 
+def parse_data(input_str: str) -> list[dict[str, str]]:
+    if not input_str:
+        return []
+
+    return [parse_line(item) for item in input_str.split("|")]
+
+
+def parse_line(input_str: str) -> dict[str, str]:
+    if not input_str:
+        return {}
+
+    return {k: v for k, v in (parse_value(item) for item in input_str.split(" "))}
+
+
+def parse_value(input_str: str) -> tuple[str, str]:
+    key, value = input_str.split("=", maxsplit=1)
+    return key, unescape(value)
+
 
 # https://github.com/benediktschmitt/py-ts3/blob/v2/ts3/escape.py
 
