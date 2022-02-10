96ab9d28e7a4e23bc8f43775896b9199d6033a01
0x4aK <honttinen@gmail.com>
2022-02-10 21:14:32 +0200

    Addition to enums

--- tsbot/enums.py
+++ tsbot/enums.py
@@ -5,3 +5,8 @@ class TextMessageTargetMode(IntEnum):
     CLIENT = 1
     CHANNEL = 2
     SERVER = 3
+
+
+class ReasonIdentifier(IntEnum):
+    REASON_KICK_CHANNEL = (4,)
+    REASON_KICK_SERVER = 5
