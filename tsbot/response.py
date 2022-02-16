1524da326b94a45d86680d3f7bb880ac3f5cea47
0x4aK <honttinen@gmail.com>
2022-02-16 12:54:32 +0200

    Rearrange, removed unnecessary call to unescape

--- tsbot/response.py
+++ tsbot/response.py
@@ -12,15 +12,15 @@ class TSResponse:
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}(data={self.data!r}, error_id={self.error_id!r}, msg={self.msg!r})"
 
+    @property
+    def first(self):
+        return self.data[0]
+
     @classmethod
     def from_server_response(cls, raw_data: list[str]):
         error_id, msg = parse_error_line(raw_data.pop())
         data = parse_data("".join(raw_data))
-        return cls(data=data, error_id=error_id, msg=unescape(msg))
-
-    @property
-    def first(self):
-        return self.data[0]
+        return cls(data=data, error_id=error_id, msg=msg)
 
 
 def parse_error_line(input_str: str) -> tuple[int, str]:
