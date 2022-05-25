7f1ff35476f30acfbd10414fad8a473aa0be93f4
0x4aK <honttinen@gmail.com>
2022-05-25 23:10:13 +0300

    Ratelimiter changes

--- tsbot/ratelimiter.py
+++ tsbot/ratelimiter.py
@@ -13,17 +13,17 @@ class RateLimiter:
         self._period: float = period
 
         self._calls: int = 0
-        self._since: float = time.monotonic_ns()
+        self._since: float = time.monotonic()
 
     async def wait(self):
-        remaining = self._period - (time.monotonic_ns() - self._since)
+        remaining = self._period - (time.monotonic() - self._since)
 
         if remaining <= 0:
             self._calls = 0
-            self._since = time.monotonic_ns()
+            self._since = time.monotonic()
 
         self._calls += 1
 
-        if self._calls > self._max_calls:
+        if self._calls >= self._max_calls:
             logger.debug("Ratelimiting, sleeping for %ss", remaining)
             await asyncio.sleep(remaining)
