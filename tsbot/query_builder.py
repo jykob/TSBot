68861fe8f28ad09ed513af6d7e3b3776dec876a3
0x4aK <honttinen@gmail.com>
2022-06-18 02:46:46 +0300

    Added TSQuery caching

--- tsbot/query_builder.py
+++ tsbot/query_builder.py
@@ -16,9 +16,13 @@ def query(command: str) -> TSQuery:
 class TSQuery:
     def __init__(self, command: str) -> None:
         if not command:
-            raise ValueError("command cannot be empty")
+            raise ValueError("Command cannot be empty")
 
         self.command = command
+
+        self._cached_command: str = ""
+        self._dirty = True
+
         self._options: list[str] = []
         self._parameters: dict[str, str] = {}
         self._parameter_blocks: list[dict[str, str]] = []
@@ -28,21 +32,31 @@ class TSQuery:
 
     def option(self: TTSQuery, *args: TStringable) -> TTSQuery:
         """Add options to the command eg. ``-groups``"""
+        self._dirty = True
+
         self._options.extend(str(arg) for arg in args)
         return self
 
     def params(self: TTSQuery, **kwargs: TStringable) -> TTSQuery:
         """Add parameters to the command eg. ``cldbid=12``"""
+        self._dirty = True
+
         self._parameters.update({k: str(v) for k, v in kwargs.items()})
         return self
 
     def param_block(self: TTSQuery, **kwargs: TStringable) -> TTSQuery:
         """Add parameter blocks eg. ``clid=1 | clid=2 | clid=3`` to the command"""
+        self._dirty = True
+
         self._parameter_blocks.append({k: str(v) for k, v in kwargs.items()})
         return self
 
     def compile(self) -> str:
         """Compiles the query into a raw command"""
+
+        if not self._dirty:
+            return self._cached_command
+
         compiled = self.command
 
         if self._parameters:
@@ -59,4 +73,5 @@ class TSQuery:
         if self._options:
             compiled += f" {' '.join(f'-{option}' for option in self._options)}"
 
+        self._cached_command, self._dirty = compiled, False
         return compiled
