158efd59dc98c49eb4a29d02acd8e327182117b1
0x4aK <honttinen@gmail.com>
2022-09-26 23:44:48 +0300

    TSEventHandler into dataclass

--- tsbot/events/tsevent_handler.py
+++ tsbot/events/tsevent_handler.py
@@ -1,23 +1,16 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from tsbot import bot, events, typealiases
 
 
+@dataclass(slots=True)
 class TSEventHandler:
-    __slots__ = "event", "handler"
-
-    def __init__(self, event: str, handler: typealiases.TEventHandler) -> None:
-        self.event = event
-        self.handler = handler
+    event: str
+    handler: typealiases.TEventHandler
 
     async def run(self, bot: bot.TSBot, event: events.TSEvent) -> None:
         await self.handler(bot, event)
-
-    def __call__(self, *args: Any, **kwargs: Any):
-        return self.run(*args, **kwargs)
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(event={self.event!r}, handler={self.handler.__qualname__!r})"
