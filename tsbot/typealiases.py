663fafe48ea8d52133ffb73f5667f8faf5c29388
0x4aK <honttinen@gmail.com>
2022-09-26 15:39:06 +0300

    Remove unused types

--- tsbot/typealiases.py
+++ tsbot/typealiases.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Awaitable, Callable, Coroutine, Protocol, TypeAlias
 
-from tsbot import bot, events, plugin
+from tsbot import bot, events
 
 
 class SupportsStr(Protocol):
@@ -13,9 +13,7 @@ class SupportsStr(Protocol):
 TCtx: TypeAlias = dict[str, str]
 
 TCommandHandler: TypeAlias = Callable[[bot.TSBot, TCtx], Awaitable[None]]
-TPluginCommandHandler: TypeAlias = Callable[[plugin.TSPlugin, bot.TSBot, TCtx], Awaitable[None]]
 
 TEventHandler: TypeAlias = Callable[[bot.TSBot, events.TSEvent], Awaitable[None]]
-TPluginEventHandler: TypeAlias = Callable[[plugin.TSPlugin, bot.TSBot, events.TSEvent], Awaitable[None]]
 
 TBackgroundTask: TypeAlias = Callable[[bot.TSBot], Coroutine[None, None, None]]
