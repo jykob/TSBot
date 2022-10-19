75311e09f265c8e974eec5ffe3a174322e7a505f
0x4aK <honttinen@gmail.com>
2022-10-19 22:27:08 +0300

    Add tasks

---
+++ tsbot/tasks/handler.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import TYPE_CHECKING
+
+logger = logging.getLogger(__name__)
+
+if TYPE_CHECKING:
+    from tsbot import tasks, bot
+
+
+class TasksHandler:
+    def __init__(self) -> None:
+        self._started = False
+        self._to_be_started: list[tasks.TSTask] = []
+        self._tasks: list[tasks.TSTask] = []
+
+    def _start_task(self, bot: bot.TSBot, task: tasks.TSTask) -> None:
+        task.task = asyncio.create_task(task.handler(bot), name=task.name)
+        task.task.add_done_callback(lambda _: self.remove_task(task))
+
+        self._tasks.append(task)
+
+    def register_task(self, bot: bot.TSBot, task: tasks.TSTask) -> None:
+        if not self._started:
+            self._to_be_started.append(task)
+        else:
+            self._start_task(bot, task)
+
+        logger.debug(f"Registered a task: {task.handler.__qualname__!r}")
+
+    def remove_task(self, task: tasks.TSTask) -> None:
+        if task.task and not task.task.done():
+            task.task.cancel()
+
+        self._tasks.remove(task)
+
+    def start(self, bot: bot.TSBot):
+
+        while self._to_be_started:
+            self._start_task(bot, self._to_be_started.pop())
+
+        self._started = True
+        logger.debug("Task handler started")
+
+    async def close(self):
+        for task in self._tasks:
+            if not task.task:
+                continue
+
+            task.task.cancel()
+
+        # Sleep until all the tasks are removed from tasks list
+        while self._tasks:
+            await asyncio.sleep(0)
