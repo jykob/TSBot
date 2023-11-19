3fca6022414886f3e5af6d1496deac9df8cee87d
0x4aK <18755570+0x4aK@users.noreply.github.com>
2023-11-19 21:59:05 +0200

    Make TSQuery objects immutable

--- tsbot/query_builder.py
+++ tsbot/query_builder.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TypeVar
+import copy
+from typing import Iterable, TypeVar
 
 from tsbot import utils
 
@@ -17,70 +18,97 @@ class TSQuery:
     __slots__ = (
         "command",
         "_cached_command",
-        "_dirty",
         "_options",
         "_parameters",
         "_parameter_blocks",
     )
 
-    def __init__(self, command: str) -> None:
+    def __init__(
+        self,
+        command: str,
+        options: list[str] | None = None,
+        parameters: dict[str, str] | None = None,
+        parameter_blocks: list[dict[str, str]] | None = None,
+    ) -> None:
         if not command:
             raise ValueError("Command cannot be empty")
 
         self.command = command
 
         self._cached_command: str = ""
-        self._dirty = True
 
-        self._options: list[str] = []
-        self._parameters: dict[str, str] = {}
-        self._parameter_blocks: list[dict[str, str]] = []
+        self._options = options or []
+        self._parameters = parameters or {}
+        self._parameter_blocks = parameter_blocks or []
 
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}({self.command!r})"
 
     def option(self: _T, *args: ParameterTypes) -> _T:
         """Add options to the command eg. ``-groups``"""
-        self._dirty = True
 
-        self._options.extend(map(str, args))
-        return self
+        return type(self)(
+            self.command,
+            [*self._options, *map(str, args)],
+            copy.copy(self._parameters),
+            copy.deepcopy(self._parameter_blocks),
+        )
 
     def params(self: _T, **kwargs: ParameterTypes) -> _T:
         """Add parameters to the command eg. ``cldbid=12``"""
-        self._dirty = True
 
-        self._parameters.update({k: str(v) for k, v in kwargs.items()})
-        return self
+        return type(self)(
+            self.command,
+            copy.copy(self._options),
+            {**self._parameters, **{k: str(v) for k, v in kwargs.items()}},
+            copy.deepcopy(self._parameter_blocks),
+        )
 
-    def param_block(self: _T, **kwargs: ParameterTypes) -> _T:
+    def param_block(
+        self: _T,
+        block: Iterable[dict[str, ParameterTypes]] | None = None,
+        /,
+        **kwargs: ParameterTypes,
+    ) -> _T:
         """Add parameter blocks eg. ``clid=1 | clid=2 | clid=3`` to the command"""
-        self._dirty = True
 
-        self._parameter_blocks.append({k: str(v) for k, v in kwargs.items()})
-        return self
+        param_blocks = tuple(block) if block else (kwargs,)
+
+        return type(self)(
+            self.command,
+            copy.copy(self._options),
+            copy.copy(self._parameters),
+            [
+                *self._parameter_blocks,
+                *({k: str(v) for k, v in block.items()} for block in param_blocks),
+            ],
+        )
 
     def compile(self) -> str:
         """Compiles the query into a raw command"""
 
-        if not self._dirty:
+        if self._cached_command:
             return self._cached_command
 
         compiled = self.command
 
         if self._parameters:
-            compiled += f" {' '.join(f'{k}={utils.escape(v)}' for k, v in self._parameters.items())}"
+            compiled += (
+                f" {' '.join(f'{k}={utils.escape(v)}' for k, v in self._parameters.items())}"
+            )
 
         if self._parameter_blocks:
             compiled_blocks: list[str] = []
 
             for parameters in self._parameter_blocks:
-                compiled_blocks.append(" ".join(f"{k}={utils.escape(v)}" for k, v in parameters.items()))
+                compiled_blocks.append(
+                    " ".join(f"{k}={utils.escape(v)}" for k, v in parameters.items())
+                )
 
             compiled += f" {'|'.join(compiled_blocks)}"
 
         if self._options:
             compiled += f" {' '.join(f'-{option}' for option in self._options)}"
 
-        self._cached_command, self._dirty = compiled, False
+        self._cached_command = compiled
         return compiled
