0793884c7c8a99a451ec011623abe99e94e0433f
0x4aK <18755570+0x4aK@users.noreply.github.com>
2023-11-24 11:20:43 +0200

    Add TSCtx, TSEvent to __init__ for type hints

--- tsbot/__init__.py
+++ tsbot/__init__.py
@@ -1,4 +1,6 @@
 from tsbot.bot import TSBot
 from tsbot.query_builder import query
+from tsbot.events import TSEvent
+from tsbot.context import TSCtx
 
-__all__ = ("TSBot", "query")
+__all__ = ("TSBot", "query", "TSEvent", "TSCtx")
