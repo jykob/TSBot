5b1c70ee8b0d27a985f930fcb81c1aa7572259f6
0x4aK <18755570+0x4aK@users.noreply.github.com>
2024-03-09 07:26:49 +0200

    Add exception example

---
+++ examples/exception_handling.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from tsbot import TSBot, TSCtx, plugin
+from tsbot.exceptions import TSCommandError
+
+
+class ExceptionHandler(plugin.TSPlugin):
+    @plugin.on("command_error")
+    async def catch_command_error(self, bot: TSBot, ctx: TSCtx):
+        await bot.respond(ctx, f"COMMAND_ERROR - {ctx['exception']!r}")
+
+    @plugin.on("permission_error")
+    async def catch_permission_error(self, bot: TSBot, ctx: TSCtx):
+        await bot.respond(ctx, f"PERMISSION_ERROR - {ctx['exception']!r}")
+
+    @plugin.on("parameter_error")
+    async def catch_parameter_error(self, bot: TSBot, ctx: TSCtx):
+        await bot.respond(ctx, f"PARAMETER_ERROR - {ctx['exception']!r}")
+
+
+bot = TSBot(
+    username="TestBot",
+    password="kK7paxA9",
+    address="localhost",
+)
+
+
+@bot.command("error", help_text="Raises exception to test handling")
+def raise_ts_exception(bot: TSBot, ctx: TSCtx):
+    raise TSCommandError("Exception raised")
+
+
+bot.load_plugin(ExceptionHandler())
+
+asyncio.run(bot.run())
