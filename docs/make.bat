572cb15be92c927cb8523a022e54541081b4114f
0x4aK <18755570+0x4aK@users.noreply.github.com>
2024-03-30 01:44:19 +0200

    docs: Update make files

--- docs/make.bat
+++ docs/make.bat
@@ -8,7 +8,8 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=source
-set BUILDDIR=build
+set BUILDDIR=_build
+set SPHINXOPTS="-c ."
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
