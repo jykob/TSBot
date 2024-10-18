c44c3c387d1834977df7f14b3a37dfa2a6c01411
jykob <18755570+jykob@users.noreply.github.com>
2024-10-18 03:29:22 +0300

    simplify tasks tracking

--- tsbot/tasks/handler.py
+++ tsbot/tasks/handler.py
@@ -14,28 +14,21 @@ logger = logging.getLogger(__name__)
 class TasksHandler:
     def __init__(self) -> None:
         self._started = False
-        self._tasks: list[tasks.TSTask] = []
+        self._tasks: set[asyncio.Task[None]] = set()
         self._starting_tasks: list[tasks.TSTask] = []
 
     def _start_task(self, bot: bot.TSBot, task: tasks.TSTask) -> None:
         task.task = asyncio.create_task(task.handler(bot), name=task.name)
-        task.task.add_done_callback(lambda _: self.remove_task(task))
-
-        self._tasks.append(task)
+        self._tasks.add(task.task)
+        task.task.add_done_callback(self._tasks.remove)
+        logger.debug("Started a task handler %r", task.handler.__name__)
 
     def register_task(self, bot: bot.TSBot, task: tasks.TSTask) -> None:
-        if not self._started:
-            self._starting_tasks.append(task)
-        else:
-            self._start_task(bot, task)
-
-        logger.debug("Registered a task handler %r", task.handler.__name__)
+        self._start_task(bot, task) if self._started else self._starting_tasks.append(task)
 
     def remove_task(self, task: tasks.TSTask) -> None:
         if task.task and not task.task.done():
             task.task.cancel()
-
-        self._tasks = list(filter(lambda t: t is not task, self._tasks))
 
     def start(self, bot: bot.TSBot) -> None:
         while self._starting_tasks:
@@ -45,8 +38,11 @@ class TasksHandler:
         logger.debug("Task handler started")
 
     async def close(self) -> None:
-        for task in filter(bool, map(lambda t: t.task, self._tasks)):
-            task.cancel()  # type: ignore
+        self._started = False
+        self._starting_tasks.clear()
+
+        for task in self._tasks:
+            task.cancel()
 
         # Sleep until all the tasks are removed from tasks list
         while self._tasks:
