634dc3ad04c923652c0be370cbf88dd77df09213
jykob <18755570+jykob@users.noreply.github.com>
2024-10-18 17:00:49 +0300

    rm unused function

--- tsbot/utils.py
+++ tsbot/utils.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import itertools
 from collections.abc import Generator
 from contextlib import contextmanager
 from typing import Any
@@ -13,12 +12,3 @@ def toggle(obj: object, attr: str, enter: Any, exit: Any) -> Generator[None, Non
         yield
     finally:
         setattr(obj, attr, exit)
-
-
-def pop_traceback(exception: Exception, amount: int = 1) -> Exception:
-    tb, count = exception.__traceback__, itertools.count()
-
-    while tb and next(count) < amount:
-        tb = tb.tb_next
-
-    return exception.with_traceback(tb)
