48030f9c875b1be5258e0a32599c7816dcd8d68d
jykob <18755570+jykob@users.noreply.github.com>
2024-10-20 00:04:32 +0300

    ratelimiter tests to async

--- tests/test_ratelimiter.py
+++ tests/test_ratelimiter.py
@@ -14,9 +14,9 @@ def default_rl():
     return ratelimiter.RateLimiter(max_calls=10, period=3)
 
 
-def test_ratelimiter_call(default_rl: ratelimiter.RateLimiter):
-    asyncio.run(default_rl.wait())
-
+@pytest.mark.asyncio
+async def test_ratelimiter_call(default_rl: ratelimiter.RateLimiter):
+    await default_rl.wait()
     assert default_rl._calls == 1
 
 
@@ -27,12 +27,12 @@ def test_ratelimiter_call(default_rl: ratelimiter.RateLimiter):
         pytest.param(8, id="test_n_of_calls_8"),
     ),
 )
-def test_ratelimiter_multiple_calls(default_rl: ratelimiter.RateLimiter, number_of_calls: int):
-    async def run_wrapper():
-        for _ in range(number_of_calls):
-            await default_rl.wait()
-
-    asyncio.run(run_wrapper())
+@pytest.mark.asyncio
+async def test_ratelimiter_multiple_calls(
+    default_rl: ratelimiter.RateLimiter, number_of_calls: int
+):
+    for _ in range(number_of_calls):
+        await default_rl.wait()
 
     assert default_rl._calls == number_of_calls
 
@@ -44,7 +44,8 @@ def test_ratelimiter_multiple_calls(default_rl: ratelimiter.RateLimiter, number_
         pytest.param(ratelimiter.RateLimiter(max_calls=10, period=1), 10, id="test_throttle_2"),
     ),
 )
-def test_ratelimiter_throttle(
+@pytest.mark.asyncio
+async def test_ratelimiter_throttle(
     monkeypatch: pytest.MonkeyPatch, rl: ratelimiter.RateLimiter, number_of_calls: int
 ):
     calls = 0
@@ -53,11 +54,9 @@ def test_ratelimiter_throttle(
         nonlocal calls
         calls += 1
 
-    async def run_wrapper():
-        for _ in range(number_of_calls):
-            await rl.wait()
-
     monkeypatch.setattr(asyncio, "sleep", mock_sleep)
-    asyncio.run(run_wrapper())
+
+    for _ in range(number_of_calls):
+        await rl.wait()
 
     assert calls != 0
