be277414d2f47c423d4b6295850d2c3a251c3d36
jykob <18755570+jykob@users.noreply.github.com>
2024-11-01 12:21:18 +0200

    add RawConnection

--- tsbot/connection/connection_types/__init__.py
+++ tsbot/connection/connection_types/__init__.py
@@ -1,3 +1,4 @@
+from tsbot.connection.connection_types.raw_connection import RawConnection
 from tsbot.connection.connection_types.ssh_connection import SSHConnection
 
-__all__ = ("SSHConnection",)
+__all__ = ("SSHConnection", "RawConnection")
---
+++ tsbot/connection/connection_types/raw_connection.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import asyncio
+
+from tsbot import parsers
+from tsbot.connection.connection_types import abc
+
+
+class RawConnection(abc.Connection):
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        address: str,
+        port: int,
+    ) -> None:
+        self._username = username
+        self._password = password
+        self._address = address
+        self._port = port
+
+        self._writer: asyncio.StreamWriter | None = None
+        self._reader: asyncio.StreamReader | None = None
+
+    async def connect(self) -> None:
+        self._reader, self._writer = await asyncio.open_connection(self._address, self._port)
+
+    async def validate_header(self) -> None:
+        if (data := await self.readline()) and data != "TS3\n\r":
+            raise ConnectionAbortedError("Invalid TeamSpeak server")
+        await self.readline()
+
+    async def authenticate(self) -> None:
+        await self.write(f"login {self._username} {self._password}")
+
+        data = await self.readline()
+        if data is None:
+            raise ConnectionAbortedError
+
+        resp = parsers.parse_line(data.rstrip().removeprefix("error "))
+        if resp["id"] != "0":
+            raise ConnectionAbortedError(resp["msg"])
+
+    def close(self) -> None:
+        if self._writer:
+            self._writer.close()
+
+    async def wait_closed(self) -> None:
+        if not self._writer:
+            raise ConnectionError("Trying to wait on uninitialized connection")
+
+        await self._writer.wait_closed()
+
+    async def write(self, data: str) -> None:
+        if not self._writer or self._writer.is_closing():
+            raise BrokenPipeError("Trying to write on a closed connection")
+
+        self._writer.write(f"{data}\n\r".encode())
+        await self._writer.drain()
+
+    async def readline(self) -> str | None:
+        if not self._reader:
+            raise ConnectionResetError("Reading on a closed connection")
+
+        try:
+            return (await self._reader.readuntil(b"\n\r")).decode()
+        except Exception:
+            return None
