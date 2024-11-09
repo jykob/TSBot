9eefda96aecae1546a28d9c40afea1d7db8494a2
jykob <18755570+jykob@users.noreply.github.com>
2024-11-09 14:43:29 +0200

    event type literals

---
+++ tsbot/events/event_types.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Literal
+
+BUILTIN_NO_CTX_EVENTS = Literal[
+    "run",
+    "connect",
+    "disconnect",
+    "reconnect",
+    "close",
+]
+
+BUILTIN_EVENTS = Literal[
+    "send",
+    "command_error",
+    "permission_error",
+    "parameter_error",
+]
+
+TS_EVENTS = Literal[
+    "textmessage",
+    "cliententerview",
+    "clientleftview",
+    "serveredited",
+    "channeledited",
+    "channeldescriptionchanged",
+    "clientmoved",
+    "channelcreated",
+    "channeldeleted",
+    "channelmoved",
+    "channelpasswordchanged",
+    "tokenused",
+]
+
+ALL_EVENTS = BUILTIN_EVENTS | BUILTIN_NO_CTX_EVENTS | TS_EVENTS | str
