e73e6ea18f765d3aaaef89e59ff56fb2712ac5a8
jykob <18755570+jykob@users.noreply.github.com>
2024-11-09 14:55:46 +0200

    bot event overloads

--- tsbot/events/event_handler.py
+++ tsbot/events/event_handler.py
@@ -2,20 +2,22 @@ from __future__ import annotations
 
 from collections.abc import Coroutine
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Protocol
+from typing import TYPE_CHECKING, Any, Protocol, TypeVar
 
 if TYPE_CHECKING:
     from tsbot import bot, events
 
+_TC = TypeVar("_TC", contravariant=True)
 
-class TEventHandler(Protocol):
-    def __call__(self, bot: bot.TSBot, ctx: Any, /) -> Coroutine[None, None, None]: ...
+
+class TEventHandler(Protocol[_TC]):
+    def __call__(self, bot: bot.TSBot, ctx: _TC, /) -> Coroutine[None, None, None]: ...
 
 
 @dataclass(slots=True)
 class TSEventHandler:
     event: str
-    handler: TEventHandler
+    handler: TEventHandler[Any]
 
     async def run(self, bot: bot.TSBot, event: events.TSEvent) -> None:
         await self.handler(bot, event.ctx)
