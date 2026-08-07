f5a4235415d60a1aa40546a168ec3948cdc17e9d
jykob <18755570+jykob@users.noreply.github.com>
2026-08-08 00:17:16 +0300

    add __hash__()

--- tsbot/query_builder/builder.py
+++ tsbot/query_builder/builder.py
@@ -58,6 +58,9 @@ class TSQuery:
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}({self._command!r})"
 
+    def __hash__(self) -> int:
+        return hash((self._command, self._options, self._parameters, self._parameter_blocks))
+
     def option(self, *args: Stringable) -> Self:
         """
         Add options to the command eg. ``-groups``.
